56186ace24fcd4a8de5d6119e8ecc3094afa4d68
Tonmoy <tonmoysutradhar2002@gmail.com>
2025-08-14 23:44:59 +0600

    MongoDB connection done.

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,37 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 7000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r8z7rex.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // await client.connect();
+    console.log("Connected to MongoDB");
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } catch (error) {
+    console.log("Error connecting to MONGODB", error);
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send(" Personal Expense Tracker Server is Running");
 });
