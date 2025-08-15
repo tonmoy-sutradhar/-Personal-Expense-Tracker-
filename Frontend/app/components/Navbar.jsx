f0de0888aad3ad99259d4b583ab13d13dae9fe04
Tonmoy <tonmoysutradhar2002@gmail.com>
2025-08-15 14:47:53 +0600

    Firebase added done for manage the user.

--- Frontend/app/components/Navbar.jsx
+++ Frontend/app/components/Navbar.jsx
@@ -25,12 +25,13 @@ function Navbar() {
           </div>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow gap-3"
           >
             <Link href={"/"}>Home</Link>
-            <Link href={"/"}>All Expenses</Link>
-            <Link href={"/"}>Add Expenses</Link>
-            <Link href={"/"}>My Expenses</Link>
+            <Link href={"/#"}>All Expenses</Link>
+            <Link href={"/#"}>Add Expenses</Link>
+            <Link href={"/#"}>My Expenses</Link>
+            <Link href={"/#"}>About Us</Link>
           </ul>
         </div>
         {/* <a className="btn btn-ghost text-xl">daisyUI</a> */}
@@ -46,16 +47,83 @@ function Navbar() {
           </div>
         </div>
       </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
+      <div className="active navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 gap-3">
           <Link href={"/"}>Home</Link>
-          <Link href={"/"}>All Expenses</Link>
-          <Link href={"/"}>Add Expenses</Link>
-          <Link href={"/"}>My Expenses</Link>
+          <Link href={"/#"}>All Expenses</Link>
+          <Link href={"/#"}>Add Expenses</Link>
+          <Link href={"/#"}>My Expenses</Link>
+          <Link href={"/#"}>About Us</Link>
         </ul>
       </div>
       <div className="navbar-end">
-        <a className="btn">Button</a>
+        {/* <button
+            title="Change theme"
+            onClick={toggleTheme}
+            className={` text-3xl mr-3 bg-[#e26fc1] font-bold transition duration-300 rounded-md shadow ${
+              theme === "light" ? "text-black " : "text-black bg-gray-950 "
+            }`}
+          >
+            {theme === "light" ? "🌙" : "🌞"}
+          </button> */}
+        {/* 
+          {user && user?.email ? (
+            <div className="flex justify-center items-center gap-3">
+              <img
+                data-tooltip-id="my-tooltip"
+                data-tooltip-content={user?.displayName}
+                className="w-11 rounded-full"
+                src={user?.photoURL}
+                alt="Profile-photo"
+              />
+              <Tooltip id="my-tooltip" place="top" />
+
+              <button onClick={logOut} className="btn bg-lime-400">
+                Logout
+              </button>
+            </div>
+          ) : (
+            <div className="flex justify-center items-center gap-3">
+              <NavLink to="/login" className="btn bg-lime-400">
+                Login
+              </NavLink>
+              <NavLink to="/register" className="btn bg-lime-400">
+                Signup
+              </NavLink>
+            </div>
+          )} */}
+
+        <div className="flex justify-center items-center gap-3">
+          {/* <img
+            // data-tooltip-id="my-tooltip"
+            // data-tooltip-content={user?.displayName}
+            className="w-11 rounded-full"
+            // src={user?.photoURL}
+            src=""
+            alt="Profile-photo"
+          /> */}
+          {/* <Tooltip id="my-tooltip" place="top" /> */}
+          {/* onClick={logOut} */}
+          <button className="btn bg-red-500 text-white rounded-3xl">
+            Logout
+          </button>
+        </div>
+
+        {/*  */}
+        <div className="flex justify-center items-center gap-3">
+          <Link
+            href="/login"
+            className="btn bg-blue-600 text-white rounded-3xl"
+          >
+            Login
+          </Link>
+          <Link
+            href="/register"
+            className="btn bg-blue-600 text-white rounded-3xl"
+          >
+            Signup
+          </Link>
+        </div>
       </div>
     </div>
   );
